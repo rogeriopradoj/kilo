53e4934a4147a1aaa6e4ca5d5cfcdc002d7f8162
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2010-11-02 16:00:29 -0200

    added functions to allow database persistence

--- kilo.js
+++ kilo.js
@@ -2,7 +2,9 @@ var jQT = $.jQTouch({
     icon: 'kilo.png',
     statusBar: 'black'
 });
+var db;
 $(document).ready(function(){
+    $('#createEntry form').submit(createEntry);
     $('#settings form').submit(saveSettings);
     $('#settings').bind('pageAnimationStart', loadSettings);
     $('#dates li a').click(function(){
@@ -14,6 +16,31 @@ $(document).ready(function(){
                                      date.getFullYear();
         refreshEntries();
     });
+    var shortName = 'Kilo';
+    var version = '1.0';
+    var displayName = 'Kilo';
+    var maxSize = 65536;
+    db = openDatabase(shortName, version, displayName, maxSize);
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql(
+                'CREATE TABLE IF NOT EXISTS entries ' +
+                '   (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+                '   date DATE NOT NULL, food TEXT NOT NULL, ' +
+                '   calories INTEGER NOT NULL );'
+            );
+        }
+    );
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql(
+                'CREATE TABLE IF NOT EXISTS errors ' +
+                '   (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+                '   code TEXT NOT NULL, ' +
+                '   message TEXT NOT NULL );'
+            );
+        }
+    );
 });
 function loadSettings() {
     $('#age').val(localStorage.age);
@@ -30,4 +57,65 @@ function saveSettings() {
 function refreshEntries() {
     var currentDate = sessionStorage.currentDate;
     $('#date h1').text(currentDate);
+    $('#date ul li:gt(0)').remove();
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql(
+                'SELECT * FROM entries WHERE date = ? ORDER BY food;',
+                [currentDate],
+                function(transaction, result) {
+                    for (var i=0; i < result.rows.length; i++) {
+                        var row = result.rows.item(i);
+                        var newEntryRow = $('#entryTemplate').clone();
+                        newEntryRow.removeAttr('id');
+                        newEntryRow.removeAttr('style');
+                        newEntryRow.data('entryId', row.id);
+                        newEntryRow.appendTo('#date ul');
+                        newEntryRow.find('.label').text(row.food);
+                        newEntryRow.find('.calories').text(row.calories);
+                        newEntryRow.find('.delete').click(function(){
+                            var clickedEntry = $(this).parent();
+                            var clickedEntryId = clickedEntry.data('entryId');
+                            deleteEntryById(clickedEntryId);
+                            clickedEntry.slideUp();
+                        });
+                    }
+                },
+                errorHandler
+            );
+        }
+    );
+}
+function createEntry() {
+    var date = sessionStorage.currentDate;
+    var calories = $('#calories').val();
+    var food = $('#food').val();
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql(
+                'INSERT INTO entries (date, calories, food) VALUES (?, ?, ?);',
+                [date, calories, food],
+                function() {
+                    refreshEntries();
+                    jQT.goBack();
+                },
+                errorHandler
+            );
+        }
+    );
+    return false;
+}
+function errorHandler(transaction, error) {
+    alert('Oops. Error was '+error.message+' (Code '+error.code+')');
+    transaction.executeSql('INSERT INTO errors (code, message) VALUES (?, ?);',
+                            [error.code, error.message]);
+    return false;
+}
+function deleteEntryById(id) {
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql('DELETE FROM entries WHERE id=?;',
+                                    [id], null, errorHandler);
+        }
+    );
 }
